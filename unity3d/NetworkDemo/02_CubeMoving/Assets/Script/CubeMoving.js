26027bed905ceff11e6968ae94b24ff059fe1245
unknown <minmin@.(none)>
2011-02-26 16:39:25 +0800

    [tab] => [    ]

--- unity3d/NetworkDemo/02_CubeMoving/Assets/Script/CubeMoving.js
+++ unity3d/NetworkDemo/02_CubeMoving/Assets/Script/CubeMoving.js
@@ -1,10 +1,10 @@
 
 function Update ()
 {
-	if (Network.isServer)
-	{
-		var moveDirection : Vector3 = new Vector3(0, 0, -1);
-		var speed : float = 10;
-		transform.Translate(speed * moveDirection * Time.deltaTime);
-	}
+    if (Network.isServer)
+    {
+        var moveDirection : Vector3 = new Vector3(0, 0, -1);
+        var speed : float = 10;
+        transform.Translate(speed * moveDirection * Time.deltaTime);
+    }
 }
