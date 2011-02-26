8855163f0c8a981308906e5ffae580f00788be1c
unknown <minmin@.(none)>
2011-02-26 18:53:39 +0800

    abc

--- unity3d/NetworkDemo/03_ChatRoom/Assets/Script/Chat.js
+++ unity3d/NetworkDemo/03_ChatRoom/Assets/Script/Chat.js
@@ -5,22 +5,22 @@ function OnGUI ()
 {
 	if (Network.isClient)
 	{
-	    GUILayout.BeginArea(Rect(0,0,300,600));
-	    GUILayout.TextArea(chatWords);
-	    GUILayout.BeginHorizontal();
-	    inputWord = GUILayout.TextArea(inputWord);
-	    if (GUILayout.Button("Send"))
-	    {
-	    	networkView.RPC("SomeoneSay", RPCMode.All, inputWord);
-	    	inputWord = "";
-	    }
-	    GUILayout.EndHorizontal();
-	    
-	  	if (GUILayout.Button("Disconnect"))
-	    {
-    	    Network.Disconnect(200);
-    	}
-	    GUILayout.EndArea();
+		GUILayout.BeginArea(Rect(0,0,300,600));
+		GUILayout.TextArea(chatWords);
+		GUILayout.BeginHorizontal();
+		inputWord = GUILayout.TextArea(inputWord);
+		if (GUILayout.Button("Send"))
+		{
+			networkView.RPC("SomeoneSay", RPCMode.All, inputWord);
+			inputWord = "";
+		}
+		GUILayout.EndHorizontal();
+
+		if (GUILayout.Button("Disconnect"))
+		{
+			Network.Disconnect(200);
+		}
+		GUILayout.EndArea();
 	}
 }
 
--- unity3d/NetworkDemo/03_ChatRoom/Assets/Script/Connection.js
+++ unity3d/NetworkDemo/03_ChatRoom/Assets/Script/Connection.js
@@ -4,19 +4,19 @@ var ServerPort : int = 2224;
 function OnGUI()
 {
 	GUILayout.BeginArea(Rect(Screen.width-200,0,200,200));
-	
+
 	if (Network.peerType == NetworkPeerType.Disconnected)
 	{
 		GUILayout.Label("Connection status: Disconnected");
-		
+
 		ServerAddr = GUILayout.TextField(ServerAddr, GUILayout.MinWidth(100));
 		ServerPort = parseInt(GUILayout.TextField(ServerPort.ToString()));
-		
-        if (GUILayout.Button("Connect as client"))
-        {
-            Network.Connect(ServerAddr, ServerPort);
-        }
-		
+
+		if (GUILayout.Button("Connect as client"))
+		{
+			Network.Connect(ServerAddr, ServerPort);
+		}
+
 		if (GUILayout.Button("Start Server"))
 		{
 			Network.InitializeServer(32, ServerPort, false);
@@ -24,20 +24,20 @@ function OnGUI()
 	}
 	else
 	{
-        if (Network.peerType == NetworkPeerType.Connecting)
-        {
-            GUILayout.Label("Connection status: Connecting");
-        }
-        else if (Network.peerType == NetworkPeerType.Client)
-        {
-        	GUILayout.Label("Connection status: Connected");
-        }
+		if (Network.peerType == NetworkPeerType.Connecting)
+		{
+			GUILayout.Label("Connection status: Connecting");
+		}
+		else if (Network.peerType == NetworkPeerType.Client)
+		{
+			GUILayout.Label("Connection status: Connected");
+		}
 		else if (Network.peerType == NetworkPeerType.Server)
 		{
 			GUILayout.Label("Connection status: Server!");
 			GUILayout.Label("Connection: " + Network.connections.length);
 		}
-		
+
 		if (GUILayout.Button("Disconnect"))
 		{
 			Network.Disconnect(200);
