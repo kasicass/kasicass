fa7c479eb40a04959df7b96b0f70ab983d8bde3e
kasicass <kasicass@gmail.com>
2013-10-23 20:13:05 +0800

    filetime

---
+++ win32/books/Win32SystemServices/chap02_Files/filetime/filetime.cpp
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include <stdio.h>
+
+void ShowTime(const char* prefix, FILETIME t)
+{
+	FILETIME ft;
+	SYSTEMTIME st;
+
+	FileTimeToLocalFileTime(&t, &ft);
+	FileTimeToSystemTime(&ft, &st);
+	printf("%s%d-%02d-%02d %02d:%02d\n", prefix, st.wYear,
+		st.wMinute, st.wDay, st.wHour, st.wMinute);
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		printf("usage: filetime.exe <file>\n");
+		return 0;
+	}
+
+	HANDLE hFile = CreateFile(argv[1], GENERIC_READ, 
+		FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		fprintf(stderr, "open failed, ecode: %u\n", GetLastError());
+		return 1;
+	}
+
+	FILETIME create, lastAccess, lastWrite;
+	BOOL success = GetFileTime(hFile, &create, &lastAccess, &lastWrite);
+	ShowTime("Create Time:      ", create);
+	ShowTime("Last Access Time: ", lastAccess);
+	ShowTime("Last Write Time:  ", lastWrite);
+	return 0;
+}
