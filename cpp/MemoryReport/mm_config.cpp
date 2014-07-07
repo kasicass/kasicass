302420377014ef5689a32a944691bbe837bd2697
Kasicass Tang <kasicass@gmail.com>
2014-07-07 11:50:34 +0800

    mm_config

---
+++ cpp/MemoryReport/mm_config.cpp
@@ -0,0 +1,21 @@
+#include "mm_config.hpp"
+
+namespace mm {
+
+static const char* const g_tagNames[] = {
+	"misc",
+	"foo",
+	"bar",
+	"vector<int>",
+	"list<int>",
+	"map",
+	"buffer",
+};
+
+const char* GetTagName(RECORD_TAG tag)
+{
+	return g_tagNames[tag];
+}
+
+}
+
