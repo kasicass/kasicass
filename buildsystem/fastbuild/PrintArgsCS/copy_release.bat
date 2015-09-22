fed35d26e0da04b4b3b2b8d4d92fe19d48b26efd
kasicass <kasicass@gmail.com>
2015-09-22 17:32:36 +0800

    PrintArgsCS

---
+++ buildsystem/fastbuild/PrintArgsCS/copy_release.bat
@@ -0,0 +1,5 @@
+copy /Y PrintArgs.exe D:\myprj\toolchain\cl.exe
+copy /Y PrintArgs.exe D:\myprj\toolchain\lib.exe
+copy /Y PrintArgs.exe D:\myprj\toolchain\link.exe
+copy /Y PrintArgs.exe D:\myprj\toolchain\rc.exe
+pause
