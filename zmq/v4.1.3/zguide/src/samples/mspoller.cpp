bb644a5a1b23669dffb96a25de7318dd9d060305
kasicass <kasicass@gmail.com>
2015-09-30 14:53:24 +0800

    mspoller/msreader

---
+++ zmq/v4.1.3/zguide/src/samples/mspoller.cpp
@@ -0,0 +1,49 @@
+// Reading from multiple sockets
+// This version uses zmq_poll()
+
+#include "zhelper.h"
+
+int main()
+{
+	// Connect to task ventilator
+	void *context = zmq_ctx_new();
+	void *receiver = zmq_socket(context, ZMQ_PULL);
+	zmq_connect(receiver, "tcp://localhost:5557");
+
+	// Connect to weather server
+	void *subscriber = zmq_socket(context, ZMQ_SUB);
+	zmq_connect(subscriber, "tcp://localhost:5556");
+	zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "10001 ", 6);
+
+	// Process messages from both sockets
+	while (1)
+	{
+		char msg[256];
+		zmq_pollitem_t items[] = {
+			{receiver, 0, ZMQ_POLLIN, 0 },
+			{subscriber, 0, ZMQ_POLLIN, 0 },
+		};
+		zmq_poll(items, 2, -1);
+		if (items[0].revents & ZMQ_POLLIN)
+		{
+			int size = zmq_recv(receiver, msg, 255, 0);
+			if (size != -1)
+			{
+				// Process task
+			}
+		}
+		if (items[1].revents & ZMQ_POLLIN)
+		{
+			int size = zmq_recv(subscriber, msg, 255, 0);
+			if (size != -1)
+			{
+				// Process weather update
+			}
+		}
+	}
+
+	zmq_close(receiver);
+	zmq_close(subscriber);
+	zmq_ctx_destroy(context);
+	return 0;
+}
---
+++ zmq/v4.1.3/zguide/src/samples/msreader.cpp
@@ -0,0 +1,54 @@
+// Reading from multiple sockets
+// This version uses a simple recv loop
+
+#include "zhelper.h"
+
+int main()
+{
+	// Connect to task ventilator
+	void *context = zmq_ctx_new();
+	void *receiver = zmq_socket(context, ZMQ_PULL);
+	zmq_connect(receiver, "tcp://localhost:5557");
+
+	// Connect to weather server
+	void *subscriber = zmq_socket(context, ZMQ_SUB);
+	zmq_connect(subscriber, "tcp://localhost:5556");
+	zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "10001 ", 6);
+
+	// Process messages from both sockets
+	// We prioritize traffic from the task ventilator
+	while (1)
+	{
+		char msg[256];
+		while (1)
+		{
+			int size = zmq_recv(receiver, msg, 255, ZMQ_DONTWAIT);
+			if (size != -1)
+			{
+				// Process task
+			}
+			else
+			{
+				break;
+			}
+		}
+		while (1)
+		{
+			int size = zmq_recv(subscriber, msg, 255, ZMQ_DONTWAIT);
+			if (size != -1)
+			{
+				// Process weather update
+			}
+			else
+			{
+				break;
+			}
+		}
+		s_sleep(1);
+	}
+
+	zmq_close(receiver);
+	zmq_close(subscriber);
+	zmq_ctx_destroy(context);
+	return 0;
+}
