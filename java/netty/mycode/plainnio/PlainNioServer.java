e0528d3a6c38f3743c3637446b811b6bb3f58b08
kasicass <kasicass@gmail.com>
2016-03-15 16:57:42 +0800

    PlainNioServer

---
+++ java/netty/mycode/plainnio/PlainNioServer.java
@@ -0,0 +1,77 @@
+package mycode.plainnio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.Selector;
+import java.nio.channels.SelectionKey;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Set;
+import java.util.Iterator;
+
+public class PlainNioServer {
+	public void serve(int port) throws IOException {
+		ServerSocketChannel serverChannel = ServerSocketChannel.open();
+		serverChannel.configureBlocking(false);
+
+		ServerSocket ss = serverChannel.socket();
+		InetSocketAddress address = new InetSocketAddress(port);
+		ss.bind(address);
+
+		Selector selector = Selector.open();
+		serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+		final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
+		for (;;) {
+			try {
+				selector.select();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				break;
+			}
+
+			Set<SelectionKey> readyKeys = selector.selectedKeys();
+			Iterator<SelectionKey> iterator = readyKeys.iterator();
+			while (iterator.hasNext()) {
+				SelectionKey key = iterator.next();
+				iterator.remove();
+
+				try {
+					if (key.isAcceptable()) {
+						ServerSocketChannel server = (ServerSocketChannel)key.channel();
+						SocketChannel client = server.accept();
+						client.configureBlocking(false);
+						client.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ, msg.duplicate());
+						System.out.println("Accepted connection from " + client);
+					}
+
+					if (key.isWritable()) {
+						SocketChannel client = (SocketChannel)key.channel();
+						ByteBuffer buffer = (ByteBuffer)key.attachment();
+						while (buffer.hasRemaining()) {
+							if (client.write(buffer) == 0) {
+								break;
+							}
+						}
+						client.close();
+					}
+				} catch (IOException ex) {
+					key.cancel();
+					try {
+						key.channel().close();
+					} catch (IOException cex) {
+						// ignore on close
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new PlainNioServer().serve(8080);
+	}
+}
