0f044414189d174dec7c5d85ee21f1f3d0fe5e21
kasicass <kasicass@gmail.com>
2016-04-13 16:35:40 +0800

    multimodule && extmodule

---
+++ py/distutils/multimodule/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='multimodule',
+  version='1.0',
+  description='multi-module test',
+  author='kasicass',
+  author_email='kasicass@gmail.com',
+  url='...',
+  packages=['foomodule', 'barmodule']
+)
