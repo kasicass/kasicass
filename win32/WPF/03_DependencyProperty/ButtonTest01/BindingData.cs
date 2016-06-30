7b4ca090e1dd361c26748f74c78efbce02020ae3
kasicass <kasicass@gmail.com>
2016-06-30 09:03:44 +0800

    WPF - DependencyProperty

---
+++ win32/WPF/03_DependencyProperty/ButtonTest01/BindingData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonTest01
+{
+    class BindingData
+    {
+        public BindingData()
+        {
+            ColorName = "Red";
+        }
+
+        private string _name = "Red";
+
+        public string ColorName
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
---
+++ win32/WPF/03_DependencyProperty/ButtonTest01/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonTest01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double _oldFontSize = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _oldFontSize = FontSize;
+        }
+
+        private void FontSizeWinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FontSize = 16;
+        }
+
+        private void FontSizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.FontSizeBtn.FontSize = 8;
+        }
+
+        private void ResetFontSizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FontSize = _oldFontSize;
+            this.FontSizeBtn.FontSize = _oldFontSize;
+        }
+    }
+}
