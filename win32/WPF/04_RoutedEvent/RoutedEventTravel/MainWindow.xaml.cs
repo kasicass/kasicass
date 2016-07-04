2b647fd9fc0e1eb85b48a4f1926b28f39b642154
kasicass <kasicass@gmail.com>
2016-07-04 11:03:51 +0800

    WPF - RoutedEvent

---
+++ win32/WPF/04_RoutedEvent/RoutedEventTravel/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEventTravel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.simpleBtn.ClassHandlerProcessed += simpleBtn_RaisedClass;
+        }
+
+        protected int eventCounter = 0;
+
+        private void InsertList(object sender, RoutedEventArgs e)
+        {
+            eventCounter++;
+            string message = "#" + eventCounter.ToString() + ":\r\n" +
+                "InsertList\r\n" +
+                "  Sender: " + sender.ToString() + "\r\n" +
+                "  Source: " + e.Source + "\r\n" +
+                "  Origianl Source: " + e.OriginalSource;
+            firstMessages.Items.Add(message);
+            e.Handled = (bool)chkHandle.IsChecked;
+        }
+
+        private void cmdClear_Click(object sender, RoutedEventArgs e)
+        {
+            eventCounter = 0;
+            firstMessages.Items.Clear();
+        }
+
+        private void simpleBtn_RaisedClass(object sender, EventArgs e)
+        {
+            eventCounter++;
+            string message = "#" + eventCounter.ToString() + ":\r\n" +
+                "Windows Class Handler\r\n" +
+                "  Sender: " + sender.ToString();
+            firstMessages.Items.Add(message);
+        }
+
+        private void ProcessHandlersToo(object sender, RoutedEventArgs e)
+        {
+            eventCounter++;
+            string message = "#" + eventCounter.ToString() + ":\r\n" +
+                "ProcessHandlersToo\r\n" +
+                "  Sender: " + sender.ToString() + "\r\n" +
+                "  Source: " + e.Source + "\r\n" +
+                "  Original Source: " + e.OriginalSource;
+            firstMessages.Items.Add(message);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            grid1.AddHandler(MySimpleButton.CustomClickEvent, new RoutedEventHandler(ProcessHandlersToo), true);
+        }
+    }
+}
---
+++ win32/WPF/04_RoutedEvent/RoutedEventTravel/MySimpleButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RoutedEventTravel
+{
+    public class MySimpleButton : Button
+    {
+        static MySimpleButton()
+        {
+            EventManager.RegisterClassHandler(typeof(MySimpleButton), CustomClickEvent, new RoutedEventHandler(CustomClickClassHandler), false);
+        }
+
+        public event EventHandler ClassHandlerProcessed;
+        public static void CustomClickClassHandler(object sender, RoutedEventArgs e)
+        {
+            MySimpleButton simpleBtn = sender as MySimpleButton;
+            EventArgs args = new EventArgs();
+            simpleBtn.ClassHandlerProcessed(simpleBtn, args);
+        }
+
+        public static readonly RoutedEvent CustomClickEvent = EventManager.RegisterRoutedEvent("CustomClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(MySimpleButton));
+        public event RoutedEventHandler CustomClick
+        {
+            add { AddHandler(CustomClickEvent, value); }
+            remove { RemoveHandler(CustomClickEvent, value); }
+        }
+
+        void RaiseCustomClickEvent()
+        {
+            RoutedEventArgs e = new RoutedEventArgs(MySimpleButton.CustomClickEvent);
+            RaiseEvent(e);
+        }
+
+        protected override void OnClick()
+        {
+            RaiseCustomClickEvent();
+        }
+    }
+}
