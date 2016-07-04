f6584f8e1d138b1fe970e9fb2678ae6d8678e076
kasicass <kasicass@gmail.com>
2016-07-04 10:20:56 +0800

    WPF - DependencyProperty

---
+++ win32/WPF/03_DependencyProperty/ButtonTest02/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonTest02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty SpaceProperty;
+        public int Space
+        {
+            set { SetValue(SpaceProperty, value); }
+            get { return (int)GetValue(SpaceProperty); }
+        }
+
+        static MainWindow()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.Inherits = true;
+            SpaceProperty = SpaceButton.SpaceProperty.AddOwner(typeof(MainWindow));
+            SpaceProperty.OverrideMetadata(typeof(Window), metadata);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSpace_Click(object sender, RoutedEventArgs e)
+        {
+            this.btnSpace.Space = 2;
+        }
+
+        private void WinSpace_Click(object sender, RoutedEventArgs e)
+        {
+            this.Space = 2;
+        }
+    }
+}
---
+++ win32/WPF/03_DependencyProperty/ButtonTest02/SpaceButton.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ButtonTest02
+{
+    public class SpaceButton : Button
+    {
+        string txt;
+
+        public string Text
+        {
+            set
+            {
+                txt = value;
+                Content = SpaceOutText(txt);
+            }
+            get
+            {
+                return txt;
+            }
+        }
+
+        public static readonly DependencyProperty SpaceProperty;
+
+        public int Space
+        {
+            set
+            {
+                SetValue(SpaceProperty, value);
+            }
+            get
+            {
+                return (int)GetValue(SpaceProperty);
+            }
+        }
+
+        static SpaceButton()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.DefaultValue = 0;
+            metadata.PropertyChangedCallback += OnSpacePropertyChanged;
+            metadata.Inherits = true;
+            SpaceProperty = DependencyProperty.Register("Space", typeof(int), typeof(SpaceButton), metadata, ValidateSpaceValue);
+        }
+
+        static bool ValidateSpaceValue(object obj)
+        {
+            int i = (int)obj;
+            return i >= 0;
+        }
+
+        static void OnSpacePropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            SpaceButton btn = obj as SpaceButton;
+            string txt = btn.Content as string;
+            if (txt == null) return;
+            btn.Content = btn.SpaceOutText(txt);
+        }
+
+        string SpaceOutText(string str)
+        {
+            if (str == null)
+                return null;
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (char ch in str)
+            {
+                builder.Append(ch + new string(' ', Space));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
