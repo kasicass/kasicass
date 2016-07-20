7eaeec79027a4ccede954aea4b6c77ed46f502b8
kasicass <kasicass@gmail.com>
2016-07-20 11:11:56 +0800

    C# Run Process

---
+++ csharp/StandardLibrary/System.Diagnostics/CreateProcessAndReadResult/RunRunRun/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace RunRunRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream stdin = System.Console.OpenStandardInput();
+            byte[] buf = new byte[128];
+            try
+            {
+                int n = stdin.Read(buf, 0, 128);
+                char[] chars = Encoding.UTF7.GetChars(buf, 0, n);
+                string s = new String(chars);
+                s = s.Trim();
+                System.Console.Write(s);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Err: " + e.Message);
+            }
+        }
+    }
+}
---
+++ csharp/StandardLibrary/System.Diagnostics/CreateProcessAndReadResult/TheMan/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace TheMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process my = new Process();
+
+            try
+            {
+                my.StartInfo.UseShellExecute = false;
+                my.StartInfo.FileName = "RunRunRun.exe";
+                my.StartInfo.CreateNoWindow = true;
+                my.StartInfo.RedirectStandardInput = true;
+                my.StartInfo.RedirectStandardOutput = true;
+                my.Start();
+
+                my.StandardInput.WriteLine("Hello Man");
+
+                char[] chars = new char[128];
+                int n = my.StandardOutput.Read(chars, 0, 128);
+                String s = new String(chars);
+                s = s.Trim();
+                System.Console.WriteLine("Output: " + s);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Err: " + e.Message);
+            }
+        }
+    }
+}
