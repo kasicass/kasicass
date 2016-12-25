04943dc46d76d9436a31fa7a7324365b7e91e5c9
kasicass <kasicass@gmail.com>
2016-12-25 11:47:17 +0800

    WPF - Handle An Event

---
+++ win32/WPF/ApplicationsCodeAndMarkup/chap01/HandleAnEvent/HandleAnEvent.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.HandleAnEvent
+{
+    class HandleAnEvent
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+
+            Window win = new Window();
+            win.Title = "Handle An Event";
+            win.MouseDown += WindowOnMouseDown;
+
+            app.Run(win);
+        }
+
+        static void WindowOnMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            Window win = sender as Window;
+            string message = string.Format("Window clicked with {0} button at point ({1})",
+                args.ChangedButton, args.GetPosition(win));
+            MessageBox.Show(message, win.Title);
+        }
+    }
+}
