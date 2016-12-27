41b311744ffe4dd22b981ee752fdc97ffc0bc12d
kasicass <kasicass@gmail.com>
2016-12-27 17:48:40 +0800

    WPF - ThrowWindowParty

---
+++ win32/WPF/ApplicationsCodeAndMarkup/chap01/ThrowWindowParty/ThrowWindowParty.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.ThrowWindowParty
+{
+    class ThrowWindowParty : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            ThrowWindowParty app = new ThrowWindowParty();
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            Window winMain = new Window();
+            winMain.Title = "Main Window";
+            winMain.MouseDown += WindowOnMouseDown;
+            winMain.Show();
+
+            for (int i = 0; i < 2; ++i)
+            {
+                Window win = new Window();
+                win.Title = "Extra Window No. " + (i+1);
+                win.Show();
+            }
+        }
+
+        void WindowOnMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            Window win = new Window();
+            win.Title = "Modal Dialog Box";
+            win.ShowDialog();
+        }
+    }
+}
