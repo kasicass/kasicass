8f8202346f2cd6c9c66c1e949f30fb119f20a11e
kasicass <kasicass@gmail.com>
2017-02-10 10:57:58 +0800

    erlang ready

---
+++ erlang/basic/hello.erl
@@ -0,0 +1,15 @@
+% hello.erl
+-export([main/1]).
+
+main([YourName]) ->
+  io:format("hello world 1~n"),
+  io:format("hello ~s~n", [YourName]);
+main([YourName, Age]) ->
+  io:format("hello world 2~n"),
+  io:format("hello ~s, age ~s~n", [YourName, Age]);
+main(_) ->
+  usage().
+
+usage() ->
+  io:format("usage: ~s name [age]~n", [filename:basename(escript:script_name())]),
+  halt(1).
