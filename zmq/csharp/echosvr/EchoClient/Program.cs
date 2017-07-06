5ed7ec08dfb1e5a13ff3ec5fc47b6459c8626976
kasicass <kasicass@gmail.com>
2017-07-06 23:27:45 +0800

    NetMQ for test

---
+++ zmq/csharp/echosvr/EchoClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RequestSocket(">tcp://localhost:5555"))
+            {
+                client.SendFrame("I'm a Message.");
+                string s = client.ReceiveFrameString();
+                Console.WriteLine("Echo: {0}", s);
+            }
+        }
+    }
+}
---
+++ zmq/csharp/echosvr/EchoServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new ResponseSocket("@tcp://localhost:5555"))
+            {
+                while (true)
+                {
+                    string s = server.ReceiveFrameString();
+                    Console.WriteLine("Recv: {0}", s);
+                    server.SendFrame(s);
+                }
+            }
+        }
+    }
+}
