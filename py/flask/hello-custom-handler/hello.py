474e71ce5ab17f9010bcf4aa201c7bc35b4d51ae
kasicass <kasicass@gmail.com>
2017-10-18 19:41:10 +0800

    hello-custom-handler

---
+++ py/flask/hello-custom-handler/hello.py
@@ -0,0 +1,27 @@
+# python -m pip install flask-script
+# python -m pip install flask-bootstrap
+# python hello.py runserver --host 127.0.0.1
+
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+@app.route("/user/<name>")
+def user(name):
+	return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+if __name__ == '__main__':
+	manager.run()
+
