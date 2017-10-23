8d54acb37f1b4f6d0c6d234a6c758254d71a9d94
kasicass <kasicass@gmail.com>
2017-10-23 19:18:14 +0800

    post/redirect/get pattern

--- py/flask/hello-ext-wtf/hello.py
+++ py/flask/hello-ext-wtf/hello.py
@@ -2,7 +2,7 @@
 # python -m pip install flask-bootstrap
 # python hello.py runserver --host 127.0.0.1
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -21,12 +21,11 @@ class NameForm(FlaskForm):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template('index.html', form=form, name=name)
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route("/user/<name>")
 def user(name):
