044daa9184982eb7c813d81f000ccee6df16e75e
kasicass <kasicass@gmail.com>
2018-01-02 22:18:21 +0800

    unconsult

--- erlang/v2/ch16/lib_misc.erl
+++ erlang/v2/ch16/lib_misc.erl
@@ -1,10 +1,15 @@
 -module(lib_misc).
--export([dump/2]).
+-export([dump/2, unconsult/2]).
 
 dump(File, Term) ->
 	Out = File ++ ".tmp",
 	io:format("** dumping ot ~s~n", [Out]),
 	{ok, S} = file:open(Out, [write]),
 	io:format(S, "~p.~n", [Term]),
+	file:close(S).
+
+unconsult(File, L) ->
+	{ok, S} = file:open(File, write),
+	lists:foreach(fun(X) -> io:format(S, "~p.~n", [X]) end, L),
 	file:close(S).
 
