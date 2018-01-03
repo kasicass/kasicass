ba0c30b7229d1bbfffe7b6982956c6e8b1ce439c
kasicass <kasicass@gmail.com>
2018-01-03 10:46:25 +0800

    url

--- erlang/v2/ch16/lib_misc.erl
+++ erlang/v2/ch16/lib_misc.erl
@@ -1,5 +1,6 @@
 -module(lib_misc).
 -export([dump/2, unconsult/2]).
+-export([ls/1]).
 
 dump(File, Term) ->
 	Out = File ++ ".tmp",
@@ -12,4 +13,17 @@ unconsult(File, L) ->
 	{ok, S} = file:open(File, write),
 	lists:foreach(fun(X) -> io:format(S, "~p.~n", [X]) end, L),
 	file:close(S).
+
+-include_lib("kernel/include/file.hrl").
+file_size_and_type(File) ->
+	case file:read_file_info(File) of
+		{ok, Facts} ->
+			{Facts#file_info.type, Facts#file_info.size};
+		_ ->
+			error
+	end.
+
+ls(Dir) ->
+	{ok, L} = file:list_dir(Dir),
+	lists:map(fun(I) -> {I, file_size_and_type(I)} end, lists:sort(L)).
 
