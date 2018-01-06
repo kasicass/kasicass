1c7ddee3d59ca25f718e83e5489e85bee113d9b7
kasicass <kasicass@gmail.com>
2018-01-06 13:13:42 +0800

    error

--- erlang/v2/ch17/lib_misc.erl
+++ erlang/v2/ch17/lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([string2value/1]).
+-export([string2value/1, sleep/1]).
 
 string2value(Str) ->
 	{ok, Tokens, _} = erl_scan:string(Str ++ "."),
@@ -7,4 +7,10 @@ string2value(Str) ->
 	Bindings = erl_eval:new_bindings(),
 	{value, Value, _} = erl_eval:exprs(Exprs, Bindings),
 	Value.
+
+sleep(T) ->
+	receive
+	after T ->
+		true
+	end.
 
--- erlang/v2/ch17/socket_examples.erl
+++ erlang/v2/ch17/socket_examples.erl
@@ -1,5 +1,12 @@
 -module(socket_examples).
--export([nano_get_url/0, nano_get_url/1, start_nano_server/0, nano_client_eval/1]).
+-export([
+	nano_get_url/0,
+	nano_get_url/1,
+	start_nano_server/0,
+	start_seq_server/0,
+	start_parallel_server/0,
+	nano_client_eval/1,
+	error_test/0]).
 -import(lists, [reverse/1]).
 
 nano_get_url() ->
@@ -47,5 +54,47 @@ nano_client_eval(Str) ->
 			Val = binary_to_term(Bin),
 			io:format("Client result = ~p~n", [Val]),
 			gen_tcp:close(Socket)
+	end.
+
+start_seq_server() ->
+	{ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4}, {reuseaddr, true}, {active, true}]),
+	seq_loop(Listen).
+
+seq_loop(Listen) ->
+	{ok, Socket} = gen_tcp:accept(Listen),
+	loop_nano_server(Socket),
+	seq_loop(Listen).
+
+start_parallel_server() ->
+	{ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4}, {reuseaddr, true}, {active, true}]),
+	spawn(fun() -> par_connect(Listen) end).
+
+par_connect(Listen) ->
+	{ok, Socket} = gen_tcp:accept(Listen),
+	spawn(fun() -> par_connect(Listen) end),
+	loop_nano_server(Socket).
+
+error_test() ->
+	spawn(fun() -> error_test_server() end),
+	lib_misc:sleep(2000),
+	{ok, Socket} = gen_tcp:connect("localhost", 4321, [binary, {packet, 2}]),
+	io:format("connected to: ~p~n", [Socket]),
+	gen_tcp:send(Socket, <<"123">>),
+	receive
+		Any ->
+			io:format("Any=~p~n", [Any])
+	end.
+
+error_test_server() ->
+	{ok, Listen} = gen_tcp:listen(4321, [binary,{packet,2}]),
+	{ok, Socket} = gen_tcp:accept(Listen),
+	error_test_server_loop(Socket).
+
+error_test_server_loop(Socket) ->
+	receive
+		{tcp, Socket, Data} ->
+			io:format("received: ~p~n", [Data]),
+			_ = atom_to_list(Data),
+			error_test_server_loop(Socket)
 	end.
 
