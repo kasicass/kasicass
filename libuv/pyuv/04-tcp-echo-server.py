d9956c4fdf280bb75057dfcf5fa741ca9191bef0
kasicass <kasicass@gmail.com>
2018-01-28 17:50:18 +0800

    echo server

---
+++ libuv/pyuv/04-tcp-echo-server.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+import signal
+import pyuv
+
+def on_read(client, data, error):
+	if data is None:
+		print('client closed:', client.getpeername())
+		client.close()
+		clients.remove(client)
+		return
+	client.write(data)
+
+def on_connection(server, error):
+	client = pyuv.TCP(server.loop)
+	server.accept(client)
+	clients.append(client)
+	client.start_read(on_read)
+	print('new client from:', client.getpeername())
+
+def signal_cb(handle, signum):
+	[c.close() for c in clients]
+	signal_h.close()
+	server.close()
+
+print("PyUV version %s" % pyuv.__version__)
+
+loop = pyuv.Loop.default_loop()
+clients = []
+
+server = pyuv.TCP(loop)
+server.bind(("0.0.0.0", 8888))
+server.listen(on_connection)
+
+signal_h = pyuv.Signal(loop)
+signal_h.start(signal_cb, signal.SIGINT)
+
+loop.run()
+print("Stopped!")
+
