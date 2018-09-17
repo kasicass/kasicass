7de8a81dafe78421e359c0e4c1a1ce67588d42c4
kasicass <kasicass@gmail.com>
2018-09-17 19:37:37 +0800

    str2num

---
+++ go/phrasebook/ch03/str2num.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var i int
+	fmt.Scanf("%d", &i)
+	str := strconv.FormatInt(int64(i), 10)
+	hex, _ := strconv.ParseInt(str, 16, 64)
+	fmt.Printf("%d\n", hex)
+}
+
