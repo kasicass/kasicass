55474cc8f9bf406957e941363e93171be518217b
kasicass <kasicass@gmail.com>
2018-09-17 20:16:07 +0800

    ptr2int

---
+++ go/phrasebook/ch03/ptr2int.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	str := "A Go variable"
+	addr := unsafe.Pointer(&str)
+	fmt.Printf("The address of str is %d\n", addr)
+	str2 := (*string)(addr)
+	fmt.Printf("String constructed from pointer: %s\n", *str2)
+	address := uintptr(addr)
+	address += 4
+	// This has undefined behavior!
+	str3 := (*string)(unsafe.Pointer(address))
+	fmt.Printf("String construct from pointer: %s\n", *str3)
+}
+
