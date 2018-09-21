62486979c551f6a7d13752723a93e2302bd0194f
kasicass <kasicass@gmail.com>
2018-09-21 18:42:28 +0800

    ch03

---
+++ go/gopl.io/src/ch03/chinese.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	ascii := 'a'
+	unicode := '国'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)
+	fmt.Printf("%d %[1]c %[1]q\n", unicode)
+	fmt.Printf("%d %[1]q\n", newline)
+}
