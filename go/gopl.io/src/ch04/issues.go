098e48bb99545c0db18b722d7aa5a4f2afc737a8
kasicass <kasicass@gmail.com>
2018-09-30 10:03:52 +0800

    issues

---
+++ go/gopl.io/src/ch04/issues.go
@@ -0,0 +1,22 @@
+// issues.exe repo:golang/go is:open json decoder
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"ch04/github"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
