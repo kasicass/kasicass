89a360e6bcf7fdffc4915a945b0bcf5280aa152a
kasicass <kasicass@gmail.com>
2018-10-03 09:20:31 +0800

    DaytimeServer

---
+++ go/gonet/src/ch03/DaytimeServer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	service := ":1200"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		daytime  := time.Now().String()
+		conn.Write([]byte(daytime))
+		conn.Close()
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
