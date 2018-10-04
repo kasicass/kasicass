a3dff39779e7598be0721279d9d4fb609d63857f
kasicass <kasicass@gmail.com>
2018-10-04 10:21:59 +0800

    UDPDaytime

---
+++ go/gonet/src/ch03/UDPDaytimeClient.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port\n", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+
+	udpAddr, err := net.ResolveUDPAddr("udp", service)
+	checkError(err)
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	checkError(err)
+
+	_, err = conn.Write([]byte("anything"))
+	checkError(err)
+
+	var buf [512]byte
+	n, err := conn.Read(buf[0:])
+	checkError(err)
+
+	fmt.Println(string(buf[0:n]))
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error", err.Error())
+		os.Exit(1)
+	}
+}
