801b8bd509dbfa897f78637d42445e8a3bdb980f
kasicass <kasicass@gmail.com>
2018-10-05 08:25:24 +0800

    Save/Load Gob

---
+++ go/gonet/src/ch04/JSONEchoClient.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+)
+
+type Person struct {
+	Name  Name
+	Email []Email
+}
+
+type Name struct {
+	Family   string
+	Personal string
+}
+
+type Email struct {
+	Kind    string
+	Address string
+}
+
+func (p Person) String() string {
+	s := p.Name.Personal + " " + p.Name.Family
+	for _, v := range p.Email {
+		s += "\n" + v.Kind + ": " + v.Address
+	}
+	return s
+}
+
+func main() {
+	person := Person{
+		Name: Name{Family: "Newmarch", Personal: "Jan"},
+		Email: []Email{
+			Email{Kind: "home", Address: "jan@newmarch.name"},
+			Email{Kind: "work", Address: "j.newmarch@boxhill.edu.au"},
+		},
+	}
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "host:port")
+		os.Exit(1)
+	}
+	service := os.Args[1]
+
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+
+	encoder := json.NewEncoder(conn)
+	decoder := json.NewDecoder(conn)
+
+	for n := 0; n < 10; n++ {
+		encoder.Encode(person)
+		var newPerson Person
+		decoder.Decode(&newPerson)
+		fmt.Println(newPerson.String())
+	}
+
+	conn.Close()
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
