8eb51e5d2b23bdd24454381bc8990d6b04c57d85
kasicass <kasicass@gmail.com>
2018-10-06 20:17:59 +0800

    UTF16Len

---
+++ go/gonet/src/ch06/UTF16Len.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf16"
+)
+
+func main() {
+	str := "百度一下，你就知道"
+
+	runes := utf16.Encode([]rune(str))
+	ints  := utf16.Decode(runes)
+	fmt.Println("UTF16 length", len(runes))
+
+	str = string(ints)
+	fmt.Println(str)
+}
