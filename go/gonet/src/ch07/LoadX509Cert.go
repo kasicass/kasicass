3bf078f5fecf3392ea4d4bec61c784ea57218113
kasicass <kasicass@gmail.com>
2018-10-07 14:15:02 +0800

    LoadX509Cert

---
+++ go/gonet/src/ch07/LoadX509Cert.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/x509"
+	"fmt"
+	"os"
+)
+
+func main() {
+	certCerFile, err := os.Open("jan.newmarch.name.cer")
+	checkError(err)
+	derBytes := make([]byte, 1000) // bigger than the file
+	count, err := certCerFile.Read(derBytes)
+	checkError(err)
+	certCerFile.Close()
+
+	cert, err := x509.ParseCertificate(derBytes[0:count])
+	checkError(err)
+
+	fmt.Printf("Name %s\n", cert.Subject.CommonName)
+	fmt.Printf("Not before %s\n", cert.NotBefore.String())
+	fmt.Printf("Not after %s\n", cert.NotAfter.String())
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
