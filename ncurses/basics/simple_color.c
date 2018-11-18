4134f98724757765781aeaf85e5f9706d7e176de
kasicass <kasicass@gmail.com>
2018-11-18 11:39:39 +0800

    simple_color

---
+++ ncurses/basics/simple_color.c
@@ -0,0 +1,54 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string);
+
+//! mode: exe
+//! int: obj
+//! flag: -Wall
+//! link: ncurses
+//! src: simple_color.c
+int main(int argc, char *argv[])
+{
+    initscr();
+    if (has_colors() == FALSE)
+    {
+        endwin();
+        printf("Your terminal does not supprot color\n");
+        exit(1);
+    }
+
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+
+    attron(COLOR_PAIR(1));
+    print_in_middle(stdscr, LINES / 2, 0, 0, "Viola !!! In color ...");
+    attroff(COLOR_PAIR(1));
+
+    getch();
+    endwin();
+
+    return 0;
+}
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string)
+{
+    int length, x, y;
+    float temp;
+
+    if (win == NULL)
+        win = stdscr;
+    getyx(win, y, x);
+
+    if (startx != 0) x = startx;
+    if (starty != 0) y = starty;
+    if (width == 0) width = 80;
+
+    length = strlen(string);
+    temp = (width - length) / 2;
+    x = startx + (int)temp;
+    mvwprintw(win, y, x, "%s", string);
+    refresh();
+}
+
